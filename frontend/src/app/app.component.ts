357094aa639c70f55c2098063471b25c19266b27
Dario A Lencina Talarico <dario.talarico@getcruise.com>
2018-04-19 00:37:53 -0400

    upgraded frontend to angular 5

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { DataService, Task } from './data.service';
-import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +6,5 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-  tasks: Observable<Task[]>;
-  
-  constructor(private dataService: DataService) {
-
-  }
-
-  ngOnInit() {
-    this.tasks = this.dataService.getTasks();
-
-    this.tasks.subscribe(tasks => console.log(tasks));
-  }
+  title = 'app';
 }
-
