7158845a07749740b5f93896cf912f860b878105
Dario A Lencina Talarico <dario.talarico@getcruise.com>
2018-04-24 10:06:39 -0400

    Connected services to UI

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import {DataService} from '../data.service';
+import {DataService, EstimateProcessConfig, Task} from '../data.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
 
   constructor(private dataService: DataService) {
-
+    // TODO (Dario): Use the real user id.
+    this.dataService.getTasks('1').subscribe(tasks => {
+      console.log(JSON.stringify(tasks));
+    });
   }
 
   ngOnInit() {
--- frontend/src/app/estimate/estimate.component.ts
+++ frontend/src/app/estimate/estimate.component.ts
@@ -1,18 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import {DataService} from '../data.service';
+import {DataService, EstimateProcessConfig} from '../data.service';
 
 @Component({
   selector: 'app-estimate',
   templateUrl: './estimate.component.html',
   styleUrls: ['./estimate.component.css']
 })
+
+// Component used to create estimate requests for contractors to provide
 export class EstimateComponent implements OnInit {
+
+  // Used to store the estimateRequest contained in this component.
+  estimateRequest: object = {};
 
   constructor(private dataService: DataService) {
 
   }
 
   ngOnInit() {
+
+  }
+
+  // TODO (Dario): Call ws to create estimate request.
+  requestEstimate(request) {
+    console.log('create request', JSON.stringify(request));
+    console.log(JSON.stringify(request));
+    const estimate: EstimateProcessConfig = new EstimateProcessConfig(
+      request.name,
+      request.assignee,
+      request.description,
+      request.duedate,
+      '1'); // TODO (DARIO): get approver id from current user.
+    this.dataService.startProcessInstance(estimate).subscribe(o => {
+      // TODO (DARIO): DO something with the response.
+      console.log(JSON.stringify(o));
+    });
+
   }
 
 }
