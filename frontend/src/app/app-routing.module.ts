9f545d09a6ad76de31c4ee55563849674a928d9e
PaulFenton <paulfenton@dal.ca>
2018-04-25 17:19:55 -0500

    integrating security features

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,17 +5,73 @@ import {MatMenuModule} from '@angular/material';
 
 import { DashboardComponent } from './secure/dashboard/dashboard.component';
 import { EstimateComponent } from './secure/estimate/estimate.component';
-import { LoginComponent } from './public/login/login.component';
+//import { LoginComponent } from './public/login/login.component';
+
 import { MakeEsimtateComponent } from './secure/make-esimtate/make-esimtate.component';
 import { AppComponent } from './app.component';
+import { HomeComponent, HomeLandingComponent } from './public/home.component';
+import { LoginComponent } from './public/auth/login/login.component';
+import { RegisterComponent } from './public/auth/register/registration.component';
+import { RegistrationConfirmationComponent, LogoutComponent } from './public/auth/confirm/confirmRegistration.component';
+import { ResendCodeComponent } from './public/auth/resend/resendCode.component';
+import { ForgotPassword2Component, ForgotPasswordStep1Component } from './public/auth/forgot/forgotPassword.component';
+import { NewPasswordComponent } from './public/auth/newpassword/newpassword.component';
+import { SecureHomeComponent } from './secure/landing/securehome.component';
 
-const appRoutes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'estimate', component: EstimateComponent },
-  { path: 'makeEstimate/:taskId', component: MakeEsimtateComponent},
-  { path: 'login', component: LoginComponent },
-  { path: '',   redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: '**', redirectTo: 'dashboard' }
+const homeRoutes: Routes = [
+  {
+      path: '',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  },
+  {
+      path: 'home',
+      component: HomeComponent,
+      children: [
+          {path: 'login', component: LoginComponent},
+          {path: 'register', component: RegisterComponent},
+          {path: 'confirmRegistration/:username', component: RegistrationConfirmationComponent},
+          {path: 'resendCode', component: ResendCodeComponent},
+          {path: 'forgotPassword/:email', component: ForgotPassword2Component},
+          {path: 'forgotPassword', component: ForgotPasswordStep1Component},
+          {path: 'newPassword', component: NewPasswordComponent},
+          {path: '', component: HomeLandingComponent}
+      ]
+  },
+];
+
+
+const secureHomeRoutes: Routes = [
+  {
+
+      path: '',
+      redirectTo: '/securehome',
+      pathMatch: 'full'
+  },
+  {
+      path: 'securehome', component: SecureHomeComponent, children: [
+        { path: 'logout', component: LogoutComponent},
+        { path: 'dashboard', component: DashboardComponent },
+        { path: 'estimate', component: EstimateComponent },
+        { path: 'makeEstimate/:taskId', component: MakeEsimtateComponent},
+      ]
+  }
+];
+
+const routes: Routes = [
+  {
+      path: '',
+      children: [
+          ...homeRoutes,
+          ...secureHomeRoutes,
+          {
+              path: '',
+              component: HomeComponent
+          }
+      ]
+  },
+
+
 ];
 
 @NgModule({
@@ -23,7 +79,7 @@ const appRoutes: Routes = [
     MatButtonModule,
     MatMenuModule,
     RouterModule.forRoot(
-      appRoutes,
+      routes,
       { enableTracing: true } // <-- debugging purposes only
     )
   ],
---
+++ frontend/src/app/public/home.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from "@angular/core";
+
+declare let AWS: any;
+declare let AWSCognito: any;
+
+@Component({
+    selector: 'awscognito-angular2-app',
+    templateUrl: './landinghome.html'
+})
+export class HomeLandingComponent {
+    constructor() {
+        console.log("HomeLandingComponent constructor");
+    }
+}
+
+@Component({
+    selector: 'awscognito-angular2-app',
+    templateUrl: './home.html'
+})
+export class HomeComponent implements OnInit {
+
+    constructor() {
+        console.log("HomeComponent constructor");
+    }
+
+    ngOnInit() {
+
+    }
+}
+
+
