9c2f6038942d581f63b94a8d79ddc32a4f79b143
PaulFenton <paulfenton@dal.ca>
2018-04-25 15:36:59 -0500

    copied relevant files from aws-cognito-angular-quickstart repo on github

---
+++ frontend/src/app/public/auth/login/login.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { UserLoginService } from "../../../service/user-login.service";
+import { ChallengeParameters, CognitoCallback, LoggedInCallback } from "../../../service/cognito.service";
+
+@Component({
+    selector: 'awscognito-angular2-app',
+    templateUrl: './login.html'
+})
+export class LoginComponent implements CognitoCallback, LoggedInCallback, OnInit {
+    email: string;
+    password: string;
+    errorMessage: string;
+    mfaStep = false;
+    mfaData = {
+        destination: '',
+        callback: null
+    };
+
+    constructor(public router: Router,
+                public userService: UserLoginService) {
+        console.log("LoginComponent constructor");
+    }
+
+    ngOnInit() {
+        this.errorMessage = null;
+        console.log("Checking if the user is already authenticated. If so, then redirect to the secure site");
+        this.userService.isAuthenticated(this);
+    }
+
+    onLogin() {
+        if (this.email == null || this.password == null) {
+            this.errorMessage = "All fields are required";
+            return;
+        }
+        this.errorMessage = null;
+        this.userService.authenticate(this.email, this.password, this);
+    }
+
+    cognitoCallback(message: string, result: any) {
+        if (message != null) { //error
+            this.errorMessage = message;
+            console.log("result: " + this.errorMessage);
+            if (this.errorMessage === 'User is not confirmed.') {
+                console.log("redirecting");
+                this.router.navigate(['/home/confirmRegistration', this.email]);
+            } else if (this.errorMessage === 'User needs to set password.') {
+                console.log("redirecting to set new password");
+                this.router.navigate(['/home/newPassword']);
+            }
+        } else { //success
+            this.router.navigate(['/securehome']);
+        }
+    }
+
+    handleMFAStep(challengeName: string, challengeParameters: ChallengeParameters, callback: (confirmationCode: string) => any): void {
+        this.mfaStep = true;
+        this.mfaData.destination = challengeParameters.CODE_DELIVERY_DESTINATION;
+        this.mfaData.callback = (code: string) => {
+            if (code == null || code.length === 0) {
+                this.errorMessage = "Code is required";
+                return;
+            }
+            this.errorMessage = null;
+            callback(code);
+        };
+    }
+
+    isLoggedIn(message: string, isLoggedIn: boolean) {
+        if (isLoggedIn) {
+            this.router.navigate(['/securehome']);
+        }
+    }
+
+    cancelMFA(): boolean {
+        this.mfaStep = false;
+        return false;   //necessary to prevent href navigation
+    }
+}
